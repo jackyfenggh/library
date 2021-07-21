3059ded9652eb08d694b768a913fc64992050475
Jacky Feng <jackyfeng1992@gmail.com>
2021-07-21 18:26:28 +1000

    Add V2 requirements

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var myLibrary = [];
+var myBooks = [
+  {author: 'test book1', title: 'test title1', pages: 'test pages1', readStatus: false}, 
+  {author: 'test book2', title: 'test title2', pages: 'test pages2', readStatus: false}
+];
 
 function Book(author, title, pages, readStatus) {
   this.author = author;
@@ -9,13 +12,21 @@ function Book(author, title, pages, readStatus) {
 
 function addBook() {
   var book = new Book('test author', 'test title', 'test pages', false);
-  myLibrary.push(book);
+  myBooks.push(book);
 }
 
 function removeBook() {
-  myLibrary.pop();
+  myBooks.pop();
 }
 
 function updateBookReadStatus(book) {
   book.readStatus = true;
+}
+
+function displayBooks() {
+  for (var i = 0; i < myBooks.length; i++) {
+    var book = document.createElement('p');
+    book.innerText = `Author: ${myBooks[i].author}, Title: ${myBooks[i].title}, Pages: ${myBooks[i].pages}, Read: ${myBooks[i].readStatus}`;
+    document.body.appendChild(book);
+  };
 }
